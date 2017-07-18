1b070d1c4116e19e88289d1853efcb8aa84deb73
Lucy Conklin <lucy.conklin@gmail.com>
2017-07-18 14:16:21 -0400

    Adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.destroy_all
+
+10.times do |i|
+  title = Faker::Hipster.word
+  description = Faker::Hipster.sentence(8)
+  deadline = DateTime.new(2017, rand(1..12), rand(1..30))
+
+  note = Note.new(title:        title,
+                  description:  description,
+                  note_type:    rand(0..1)
+                  )
+
+  note.deadline = deadline if note.note_type == "goal"
+  note.save
+end
+
+p "Created #{Note.count} notes"
