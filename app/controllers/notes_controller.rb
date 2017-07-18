d93952afebb1d031e71af5778780dc652c445fa3
Lucy Conklin <lucy.conklin@gmail.com>
2017-07-18 11:09:04 -0400

    Adds controller method, view, and errors for new notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,26 @@
 class NotesController < ApplicationController
 
   def index
+    @notes = Note.all
   end
 
   def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      flash[:success] = "You successfully created a new note."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :description, :deadline, :note_type)
   end
 end
