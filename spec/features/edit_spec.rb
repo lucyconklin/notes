e0ee4643b0169d5288b278719e3a1fe3e5536f38
Lucy Conklin <lucy.conklin@gmail.com>
2017-07-18 13:05:01 -0400

    Adds tests and uses partial for edit view

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -1,11 +1,85 @@
 require 'rails_helper'
 
 feature 'As a user, I can edit notes' do
-  xscenario 'from the index page' do
-
+  before :each do
+    @note = Note.create(title: 'This is a title',
+                       description: 'this is a description',
+                       note_type: 'goal',
+                       deadline: DateTime.now
+                       )
   end
 
-  xscenario 'from the show page' do
-    
+  scenario 'from the index page' do
+    visit root_path
+
+    expect(page).to have_content(:link_or_button, 'Edit')
+  end
+
+  scenario 'from the show page' do
+    visit note_path(@note)
+
+    expect(page).to have_content(:link_or_button, 'Edit')
+  end
+
+  scenario 'I can edit all fields' do
+    visit edit_note_path(@note)
+
+    expect(page).to have_content('edit note')
+    expect(page).to have_content(@note.description)
+    expect(page).to have_content(@note.note_type)
+  end
+
+  scenario 'I can save a valid note' do
+    title = "Something new"
+    description = "New stuff"
+    deadline = DateTime.new(2001,2,3)
+
+    visit edit_note_path(@note)
+
+    fill_in 'Title', with: title
+    fill_in 'Description', with: description
+    fill_in 'Deadline', with: deadline
+    click_on 'Save Note'
+
+    expect(page).to have_content(:link_or_button, 'Edit')
+    expect(page).to have_content(title)
+    expect(page).to have_content(description)
+    expect(page).to have_content(@note.note_type)
+    expect(page).to have_content('2001-02-03')
+
+    expect(page).not_to have_content('This is a title')
+    expect(page).not_to have_content('this is a description')
+  end
+
+  scenario 'invalid titles cannot be saved' do
+    title = ""
+    description = "New stuff"
+    deadline = DateTime.new(2001,2,3)
+
+    visit edit_note_path(@note)
+
+    fill_in 'Title', with: title
+    fill_in 'Description', with: description
+    fill_in 'Deadline', with: deadline
+    click_on 'Save Note'
+
+    expect(page).to have_content('edit note')
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  scenario 'invalid deadlines cannot be saved' do
+    title = "Title"
+    description = "New stuff"
+    deadline = ""
+
+    visit edit_note_path(@note)
+
+    fill_in 'Title', with: title
+    fill_in 'Description', with: description
+    fill_in 'Deadline', with: deadline
+    click_on 'Save Note'
+
+    expect(page).to have_content('edit note')
+    expect(page).to have_content("Deadline can't be blank")
   end
 end
